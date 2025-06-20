8e197006a3f9070d18173fa10f81c34aeda77bd7
Seu Nome <seu@email.com>
2025-06-20 13:54:13 -0300

    first commit

---
+++ DTOs/SignalUploadDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BmFP_API.DTOs;
+
+public class SignalUploadDTO
+{
+    public IFormFile File { get; set; }
+    public string SignalType { get; set; }
+    public double SampleRate { get; set; }
+    
+}
