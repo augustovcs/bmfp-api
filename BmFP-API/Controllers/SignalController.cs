1ba815d4eb29ad52e8b164c48b25571e6c1b89b2
Seu Nome <seu@email.com>
2025-06-21 07:08:38 -0300

    Fix: Make BmFP-API a regular folder instead of a Git submodule

---
+++ BmFP-API/Controllers/SignalController.cs
@@ -0,0 +1,20 @@
+﻿using BmFP_API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BmFP_API.Controllers;
+
+
+[ApiController]
+[Route("api/signal/[controller]")]
+public class SignalController : ControllerBase
+{
+    
+    [HttpPost]
+    public ActionResult<string> UploadSignal([FromForm] SignalUploadDTO dto)
+    {
+        
+        return "ok";
+        
+    }
+    
+}
